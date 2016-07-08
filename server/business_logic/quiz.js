bcff2ab7ff845dd6ad7a29004f8fe87020724d1c
Eddy Fishman <eddy.fishman@gmail.com>
2016-07-08 14:36:02 +0300

    fb posts

--- server/business_logic/quiz.js
+++ server/business_logic/quiz.js
@@ -197,7 +197,7 @@ function setPostStory(data, story, objectData) {
   }
 
   if (replaced && data.session.quiz.serverData.share.story.facebookPost && data.session.quiz.serverData.share.story.facebookPost.object && objectData) {
-    var openGraphObject = commonBusinessLogic.getOpenGraphObject(data.session.quiz.serverData.share.story.facebookPost, objectData, false, data.session.clientInfo.mobile);
+    var openGraphObject = commonBusinessLogic.getOpenGraphObject(data.session.quiz.serverData.share.story.facebookPost.object.name, objectData, false, data.session.clientInfo.mobile);
     data.session.quiz.serverData.share.story.facebookPost.object = openGraphObject.facebookObject;
   }
 
